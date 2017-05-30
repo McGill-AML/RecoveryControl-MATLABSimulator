d8cd79a2deed6f89b0f14485527749f873233b4e
joshlevin88 <joshua.levin@mail.mcgill.ca>
2017-05-30 14:50:42 -0400

    found solutions

--- circle_line_minimum_distance.m
+++ circle_line_minimum_distance.m
@@ -1,17 +1,17 @@
-N = [1 1 0];
-N = N/norm(N);
-C = [2 0 0];
-M = [0 0 1];
-M = M/norm(M);
+% N = [1 1 0];
+% N = N/norm(N);
+% C = [2 0 0];
+% M = [0 0 1];
+% M = M/norm(M);
 r = 0.5;
 tic
 for j = 1
 % answer should be 1.5
-    syms t
+    syms t N M C
     
     H = (norm(cross(N,M))^2*t^2 + 2*dot(cross(N,M),cross(N,-C))*t + norm(cross(N,-C))^2)*(t+dot(M,-C))^2 ...
          - r^2*(norm(cross(N,M))^2*t + dot(cross(N,M),cross(N,-C)))^2;
-
+%%
     coeffs = sym2poly(H);
 
     soln = double(roots(coeffs));
