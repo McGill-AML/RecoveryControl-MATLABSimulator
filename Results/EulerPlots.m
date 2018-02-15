c29cfe2bf2f3cc6e8cc3c78d3002f24b2530d77f
Pulkit Rustagi <rustagi.pulkit@gmail.com>
2018-02-15 16:05:47 +0530

    Adding script for plotting experiment vs simulation data in colorplots.m

--- Results/EulerPlots.m
+++ Results/EulerPlots.m
@@ -4,7 +4,7 @@ grid on
 numPitch = 46;
 numOffset = 71;
 for iPitch = 1%:numPitch
-    for iOffset = 1%:numOffset
+    for iOffset = 2%:numOffset
         trial = 71*(iPitch-1)+iOffset;
         disp(trial);
         offset = cell2mat(Batch(trial,1));
@@ -14,13 +14,15 @@ for iPitch = 1%:numPitch
         deflections = cell2mat(Batch(trial,7));
         recoveryStages = cell2mat(Batch(trial,8));
         states = cell2mat(Batch(trial,11));
-        EulerAngles = cell2mat(Batch(trial,12));
+        RPM = cell2mat(Batch(trial,12));
+        U = cell2mat(Batch(trial,13));
+        EulerAngles = cell2mat(Batch(trial,14));
         phi = EulerAngles(1,:);
         theta = EulerAngles(2,:);
         psi = EulerAngles(3,:);
     end
 end
- 
+%% Euler Angles
 % plot(times(:),phi(:));
 % plot(times(:),theta(:));
 % plot(times(:),psi(:));
@@ -28,8 +30,38 @@ end
 % title(strcat('Euler Angles (\phi, \theta, \psi) for Pitch = ',num2str(-pitch),'^o'));
 % xlabel('Time(s)');
 % ylabel('EulerAngles(rad)');
+ 
+%% Propeller RPMs
+% plot(times(:),RPM(:,1));
+% plot(times(:),RPM(:,2));
+% plot(times(:),RPM(:,3));
+% plot(times(:),RPM(:,4));
+% legend('\Omega_1','\Omega_2', '\Omega_3', '\Omega_4');
+% title(strcat('RPMs for Pitch = ',num2str(-pitch),'^o'));
+% xlabel('Time(s)');
+% ylabel('RPM(rad/s)');
+
+%% Control Inputs U1,U2,U3,U4
+% plot(times(:),U(:,1));
+% legend('U_1');
+% title(strcat('Thrust for Pitch = ',num2str(-pitch),'^o'));
+% xlabel('Time(s)');
+% ylabel('Input Thrust(N)');
+% saveas(gcf,'Control Thrust.jpg');
+% figure();
+% hold on;
+% grid on;
+% plot(times(:),U(:,2));
+% plot(times(:),U(:,3));
+% plot(times(:),U(:,4));
+% legend('U_2(M_x)', 'U_3(M_y)', 'U_4(M_z)');
+% title(strcat('Desired Moments for Pitch = ',num2str(-pitch),'^o'));
+% xlabel('Time(s)');
+% ylabel('Desired Moments (Nm)');
+% saveas(gcf,'Control Moments.jpg');
 
 
+%% Height Loss
 % floor=-ones(1,401);
 % plot(times(:),states(9,:));
 % plot(times(:),floor(:),'r-');
--- Results/PoleNormalForceArrows.m
+++ Results/PoleNormalForceArrows.m
@@ -1,18 +1,58 @@
+%% Scaled down force magnitude Arrows plotted on the pole
+% hold on;
+% global poleRadius
+% numPitch = 46;
+% numOffset = 71;
+% height = 3.0;
+% axis('equal');
+% axis([-.2,.2,-.2,.2]);
+% grid on;
+% n = 100;
+% [X,Y,Z] = cylinder(poleRadius,n);
+% Z = [zeros(1,n+1); height*ones(1,n+1)];
+% polePoints = [X;Y;Z];
+% surf(X,Y,Z);
+% for iPitch = 1%:numPitch     % poleNormal plot for 15 degree Pitch
+%     for iOffset = 1:numOffset
+%         trial = 71*(iPitch-1)+iOffset;
+%         disp(trial);
+%         offset = cell2mat(Batch(trial,1));
+% %         pitch = cell2mat(Batch(trial,2));
+% %         success = cell2mat(Batch(trial,3));
+%         normal = cell2mat(Batch(trial,4));
+%         contactPoint = cell2mat(Batch(trial,5))*poleRadius;
+% %     	positions = cell2mat(Batch(trial,6));
+% %     	deflections = cell2mat(Batch(trial,7));
+% %     	recoveryStages = cell2mat(Batch(trial,8));
+% %         states = cell2mat(Batch(trial,9));
+%        normalForces = cell2mat(Batch(trial,12));
+%        ImpactBumper = cell2mat(Batch(trial,13));
+% %        ImpactBumperFirst = min([find(ImpactBumper(:,1),1),find(ImpactBumper(:,2),1),find(ImpactBumper(:,3),1),find(ImpactBumper(:,4),1)]);
+%         hold on;
+%         Force =normal*max([max(normalForces(1,:)),max(normalForces(2,:)),max(normalForces(3,:)),max(normalForces(4,:))]);
+%         Force=Force./500;
+%         quiver3(contactPoint(1),contactPoint(2),height/2,Force(1),Force(2),Force(3),'MaxHeadSize',0.6,'LineWidth',1);
+%         
+%     end
+% end
+
+%% Normal directions plotted on the pole using offset colour map
 hold on;
 global poleRadius
 numPitch = 46;
 numOffset = 71;
 height = 3.0;
 axis('equal');
-axis([-.2,.2,-.2,.2]);
+axis([-.2,.11,-.2,.2]);
 grid on;
 n = 100;
 [X,Y,Z] = cylinder(poleRadius,n);
 Z = [zeros(1,n+1); height*ones(1,n+1)];
 polePoints = [X;Y;Z];
 surf(X,Y,Z);
+map=[];
 for iPitch = 1%:numPitch     % poleNormal plot for 15 degree Pitch
-    for iOffset = 1:numOffset
+    for iOffset = 1:2:numOffset
         trial = 71*(iPitch-1)+iOffset;
         disp(trial);
         offset = cell2mat(Batch(trial,1));
@@ -28,9 +68,15 @@ for iPitch = 1%:numPitch     % poleNormal plot for 15 degree Pitch
        ImpactBumper = cell2mat(Batch(trial,13));
 %        ImpactBumperFirst = min([find(ImpactBumper(:,1),1),find(ImpactBumper(:,2),1),find(ImpactBumper(:,3),1),find(ImpactBumper(:,4),1)]);
         hold on;
-        Force =normal*max([max(normalForces(1,:)),max(normalForces(2,:)),max(normalForces(3,:)),max(normalForces(4,:))]);
-        Force=Force./500;
-        quiver3(contactPoint(1),contactPoint(2),height/2,Force(1),Force(2),Force(3),'MaxHeadSize',0.6,'LineWidth',1);
+        color = [abs((offset+1)/2) 0 0];
+        map=[map;color];
+        Force =normal;%*max([max(normalForces(1,:)),max(normalForces(2,:)),max(normalForces(3,:)),max(normalForces(4,:))]);
+        Force=Force./10;
+        quiver3(contactPoint(1),contactPoint(2),height/2,Force(1),Force(2),Force(3),'MaxHeadSize',0.3,'LineWidth',1.0,'color',color);
         
     end
 end
+colormap(map);
+caxis([-1 1]);
+colormap(map);
+colorbar;
